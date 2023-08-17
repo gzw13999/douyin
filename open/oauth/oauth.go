ad78adfb91061597fb04c1f30f855abd30d186ca
gzw13999 <gzw13999@126.com>
2023-08-18 02:30:24 +0800

    新增renew_refresh_token 和 refresh_token 接口

--- open/oauth/oauth.go
+++ open/oauth/oauth.go
@@ -14,8 +14,10 @@ const (
 	redirectOauthURL      string = "https://open.douyin.com/platform/oauth/connect?client_key=%s&response_type=code&scope=%s&redirect_uri=%s&state=%s"
 	silenceOauthURL       string = "https://open.douyin.com/platform/oauth/authorize/v2?client_key=%s&response_type=code&scope=login_id&redirect_uri=%s&state=%s"
 	accessTokenURL        string = "https://open.douyin.com/oauth/access_token?client_key=%s&client_secret=%s&code=%s&grant_type=authorization_code"
-	refreshAccessTokenURL string = "https://open.douyin.com/oauth/oauth/refresh_token?client_key=%s&grant_type=refresh_token&refresh_token=%s"
-	clientTokenURL        string = "https://open.douyin.com/oauth/oauth/client_token/?client_key=%s&client_secret=%s&grant_type=client_credential"
+	refreshAccessTokenURL string = "https://open.douyin.com/oauth/refresh_token?client_key=%s&grant_type=refresh_token&refresh_token=%s"
+	//refreshTokenURL       string = "https://open.douyin.com/oauth/oauth/refresh_token?client_key=%s&grant_type=refresh_token&refresh_token=%s"
+	renewRefreshTokenURL string = "https://open.douyin.com/oauth/renew_refresh_token?client_key=%s&refresh_token=%s"
+	clientTokenURL       string = "https://open.douyin.com/oauth/client_token/?client_key=%s&client_secret=%s&grant_type=client_credential"
 )
 
 // Oauth 保存用户授权信息
@@ -77,5 +79,58 @@ func (oauth *Oauth) GetUserAccessToken(code string) (accessToken credential.Acce
 		return
 	}
 	accessToken = result.Data
+	return
+}
+
+// RefreshAccessToken 刷新AccessToken.
+// 当access_token过期（过期时间15天）后，可以通过该接口使用refresh_token（过期时间30天）进行刷新
+func (oauth *Oauth) RefreshAccessToken(refreshToken string) (accessToken credential.AccessToken, err error) {
+	uri := fmt.Sprintf(refreshAccessTokenURL, oauth.ClientKey, refreshToken)
+	var response []byte
+	response, err = util.HTTPGet(uri)
+	if err != nil {
+		return
+	}
+	var result accessTokenRes
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+
+	if result.Data.ErrCode != 0 {
+		err = fmt.Errorf("GetUserAccessToken error : errcode=%v , errmsg=%v", result.Data.ErrCode, result.Data.ErrMsg)
+		return
+	}
+
+	accessToken = result.Data
+	return
+}
+
+type refreshTokenRes struct {
+	Message string                  `json:"message"`
+	Data    credential.RefreshToken `json:"data"`
+}
+
+// RenewRefreshToken 刷新refresh_token.
+// 前提： client_key需要具备renew_refresh_token这个权限
+// 接口说明： 可以通过旧的refresh_token获取新的refresh_token,调用后旧refresh_token会失效，新refresh_token有30天有效期。最多只能获取5次新的refresh_token，5次过后需要用户重新授权。
+func (oauth *Oauth) RenewRefreshToken(refreshToken string) (refreshTokenData credential.RefreshToken, err error) {
+	uri := fmt.Sprintf(renewRefreshTokenURL, oauth.ClientKey, refreshToken)
+	var response []byte
+	response, err = util.HTTPGet(uri)
+	if err != nil {
+		return
+	}
+	var result refreshTokenRes
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+
+	if result.Data.ErrCode != 0 {
+		err = fmt.Errorf("RenewRefreshToken error : errcode=%v , errmsg=%v", result.Data.ErrCode, result.Data.ErrMsg)
+		return
+	}
+	refreshTokenData = result.Data
 	return
 }
--- open/video/video.go
+++ open/video/video.go
@@ -294,10 +294,10 @@ type ListInfo struct {
 	HasMore bool  `json:"has_more"`
 	List    []struct {
 		Statistics struct {
-			DiggCount     int32 `json:"digg_count"`     //	点赞数
+			DiggCount     int32 `json:"digg_count"`     //点赞数
 			DownloadCount int32 `json:"download_count"` //下载数
 			PlayCount     int32 `json:"play_count"`     //播放数，只有作者本人可见。公开视频设为私密后，播放数也会返回0。
-			ShareCount    int32 `json:"share_count"`    //	分享数
+			ShareCount    int32 `json:"share_count"`    //分享数
 			ForwardCount  int32 `json:"forward_count"`  //转发数
 			CommentCount  int32 `json:"comment_count"`  //评论数
 		} `json:"statistics"`
